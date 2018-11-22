5ca3b9d279374c7c756705a8c9e675f2b326302f
crashbone <okanakar@std.iyte.edu.tr>
2018-11-23 00:45:18 +0300

    Add files via upload

--- black-friday/index.js
+++ black-friday/index.js
@@ -2,7 +2,7 @@ var app = new Vue({
     el: '#maindiv',
     data: {
         test: "asd",
-        items:[
+        items: [
             // [
             //     (0)"IMGPATH",
             //     (1)"TITLE",
@@ -10,40 +10,28 @@ var app = new Vue({
             //     (3)"http://link"
             // ]
             [
-                "./img/kulaklik/pio-beyaz.jpg",
-                "[TR]Pioneer Cuffia Se-Mj503T-W Kulaklık, Beyaz",
-                "48,20 TL",
-                "https://www.amazon.com.tr/Pioneer-Cuffia-Se-Mj503T-W-Kulaklık-Beyaz/dp/B015RLEAH0"
+                "./img/ekran-karti/msi1050ti.jpg",
+                '[TR]MSI 1050 TI 4GB AERO',
+                "940,64 TL",
+                "https://www.amazon.com.tr/dp/B073WGWCDC"
             ],
             [
-                "./img/kulaklik/pio-siyah.jpg",
-                "[TR]Pioneer Se-Mj503-T-K Kulaklık, Siyah",
-                "57,20 TL",
-                "https://www.amazon.com.tr/Pioneer-SE-MJ503T-K-Se-Mj503-T-K-Kulaklık-Siyah/dp/B015RLE9HQ"
+                "./img/kulaklik/jbl-t450.jpg",
+                '[HB]JBL T450 Kulaküstü Kulaklık CT OE',
+                "99,90 TL",
+                "https://www.hepsiburada.com/jbl-t450-kulakustu-kulaklik-ct-oe-pm-HB000004FXQO"
             ],
-			[
-                "./img/ssd/samsung.jpg",
-                "[COM]Samsung 860 EVO 500GB",
-                "$72.99 + $20.33",
-                "https://www.amazon.com/Samsung-500GB-Internal-MZ-76E500B-AM/dp/B0781Z7Y3S"
+            [
+                "./img/fare/deathadder6400.jpg",
+                '[HB]Razer Deathadder 6400DPI',
+                "199,00 TL",
+                "https://www.hepsiburada.com/razer-deathadder-expert-mouse-p-BD800351"
             ],
-			[
-                "./img/ssd/samsung.jpg",
-                "[COM]Samsung 860 EVO 1TB",
-                "$127.98 + $31.14",
-                "https://www.amazon.com/Samsung-Inch-Internal-MZ-76E1T0B-AM/dp/B078DPCY3T"
-            ],
-			[
-                "./img/controller/ps4.jpg",
-                "[COM]Sony PS4 Dualshock 4 Wireless Controller",
-                "$38.99 + $16.12",
-                "https://www.amazon.com/DualShock-Wireless-Controller-PlayStation-Black-4/dp/B01LWVX2RG"
-            ],
-			[
-                "./img/ekran-karti/asus1070ti.jpg",
-                "[HB]Asus Turbo 1070 TI 8GB",
-                "2.319,15 TL",
-                "https://www.hepsiburada.com/asus-turbo-nvidia-geforce-gtx-1070ti-8gb-256bit-gddr5-dx12-pci-e-3-0-ekran-karti-turbo-gtx1070ti-8g-p-HBV000009813B?"
+            [
+                "./img/fare/deathadder1800.jpg",
+                '[GB]Razer Deathadder RZ01-0085 1800DPI',
+                "131.11 TL",
+                "https://tr.gearbest.com/mouse/pp_1422191.html"
             ],
             [
                 "./img/monitor/vx2458.jpg",
@@ -52,31 +40,41 @@ var app = new Vue({
                 "https://www.hepsiburada.com/viewsonic-vx2458-c-mhd-24-144hz-1ms-hdmi-display-dvi-freesync-full-hd-curved-monitor-p-HBV00000F4VYS"
             ],
             [
-                "./img/fare/deathadder1800.jpg",
-                '[GB]Razer Deathadder RZ01-0085 1800DPI',
-                "131.11 TL",
-                "https://tr.gearbest.com/mouse/pp_1422191.html"
+                "./img/ekran-karti/asus1070ti.jpg",
+                "[HB]Asus Turbo 1070 TI 8GB",
+                "2.319,15 TL",
+                "https://www.hepsiburada.com/asus-turbo-nvidia-geforce-gtx-1070ti-8gb-256bit-gddr5-dx12-pci-e-3-0-ekran-karti-turbo-gtx1070ti-8g-p-HBV000009813B?"
             ],
-			[
-                "./img/fare/deathadder6400.jpg",
-                '[HB]Razer Deathadder 6400DPI',
-                "199,00 TL",
-                "https://www.hepsiburada.com/razer-deathadder-expert-mouse-p-BD800351"
+            [
+                "./img/controller/ps4.jpg",
+                "[COM]Sony PS4 Dualshock 4 Wireless Controller",
+                "$38.99 + $16.12",
+                "https://www.amazon.com/DualShock-Wireless-Controller-PlayStation-Black-4/dp/B01LWVX2RG"
             ],
-			[
-                "./img/kulaklik/jbl-t450.jpg",
-                '[HB]JBL T450 Kulaküstü Kulaklık CT OE',
-                "99,90 TL",
-                "https://www.hepsiburada.com/jbl-t450-kulakustu-kulaklik-ct-oe-pm-HB000004FXQO"
+            [
+                "./img/ssd/samsung.jpg",
+                "[COM]Samsung 860 EVO 1TB",
+                "$127.98 + $31.14",
+                "https://www.amazon.com/Samsung-Inch-Internal-MZ-76E1T0B-AM/dp/B078DPCY3T"
             ],
-			[
-                "./img/ekran-karti/msi1050ti.jpg",
-                '[TR]MSI 1050 TI 4GB AERO',
-                "940,64 TL",
-                "https://www.amazon.com.tr/dp/B073WGWCDC"
+            [
+                "./img/ssd/samsung.jpg",
+                "[COM]Samsung 860 EVO 500GB",
+                "$72.99 + $20.33",
+                "https://www.amazon.com/Samsung-500GB-Internal-MZ-76E500B-AM/dp/B0781Z7Y3S"
             ],
-			
-			
+            [
+                "./img/kulaklik/pio-siyah.jpg",
+                "[TR]Pioneer Se-Mj503-T-K Kulaklık, Siyah",
+                "57,20 TL",
+                "https://www.amazon.com.tr/Pioneer-SE-MJ503T-K-Se-Mj503-T-K-Kulaklık-Siyah/dp/B015RLE9HQ"
+            ],
+            [
+                "./img/kulaklik/pio-beyaz.jpg",
+                "[TR]Pioneer Cuffia Se-Mj503T-W Kulaklık, Beyaz",
+                "48,20 TL",
+                "https://www.amazon.com.tr/Pioneer-Cuffia-Se-Mj503T-W-Kulaklık-Beyaz/dp/B015RLEAH0"
+            ],
         ]
     }
 })
