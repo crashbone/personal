eedeacb694db14c1640fa66e9e5eb2a8e82e1b66
crashbone <okanakar@std.iyte.edu.tr>
2018-11-22 17:39:11 +0300

    Add files via upload

--- black-friday/index.js
+++ black-friday/index.js
@@ -23,19 +23,19 @@ var app = new Vue({
             ],
 			[
                 "./img/ssd/samsung.jpg",
-                "Samsung 860 EVO 500GB",
+                "[COM]Samsung 860 EVO 500GB",
                 "$72.99 + $20.33",
                 "https://www.amazon.com/Samsung-500GB-Internal-MZ-76E500B-AM/dp/B0781Z7Y3S"
             ],
 			[
                 "./img/ssd/samsung.jpg",
-                "Samsung 860 EVO 1TB",
+                "[COM]Samsung 860 EVO 1TB",
                 "$127.98 + $31.14",
                 "https://www.amazon.com/Samsung-Inch-Internal-MZ-76E1T0B-AM/dp/B078DPCY3T"
             ],
 			[
                 "./img/controller/ps4.jpg",
-                "Sony PS4 Dualshock 4 Wireless Controller",
+                "[COM]Sony PS4 Dualshock 4 Wireless Controller",
                 "$38.99 + $16.12",
                 "https://www.amazon.com/DualShock-Wireless-Controller-PlayStation-Black-4/dp/B01LWVX2RG"
             ],
