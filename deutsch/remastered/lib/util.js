6416f81d1624d61c02cb379ad4a22fc6124f6864
okan <okanakar@std.iyte.edu.tr>
2019-08-25 12:23:51 +0300

    Add files via upload

--- deutsch/remastered/lib/util.js
+++ deutsch/remastered/lib/util.js
@@ -28,6 +28,12 @@ var util = {
                 return item.split(expression);
             })
         },
+        readFileFromServer(url){
+      		var xmlHttp = new XMLHttpRequest()
+      		xmlHttp.open( "GET", url, false ) // false for synchronous request
+      		xmlHttp.send( null )
+      		return xmlHttp.responseText
+      	}
 
     },
 }
