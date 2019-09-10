86263ea287b9e4a39e53b9dec5d8dddb97865c3e
okan <okanakar@std.iyte.edu.tr>
2019-09-10 17:53:42 +0300

    Add files via upload

--- mbw/item-prices/js/1.js
+++ mbw/item-prices/js/1.js
@@ -10,7 +10,7 @@ var app = new Vue({
       sortCounter: 0,
       flatSorted: false,
       percentageSorted: false,
-      items:[],
+      items: [],
 
       flatMin: 125,
       flatMax: 200,
@@ -19,113 +19,80 @@ var app = new Vue({
 
       language: "tr",
       dict: {
-
-         "Tools":{
+         // ===============================================================
+         // ============================ ITEMS ============================
+         // ===============================================================
+         "Tools": {
             tr: "Aletler",
          },
-         "Linen":{
+         "Linen": {
             tr: "Keten",
          },
-         "Leatherwork":{
+         "Leatherwork": {
             tr: "İşlenmiş deri",
          },
-
-         "Hides":{
+         "Hides": {
             tr: "Deri",
          },
-
-         "Wool Cloth":{
+         "Wool Cloth": {
             tr: "Pamuk Kumaş",
          },
-
-         "Pottery":{
+         "Pottery": {
             tr: "Çömlek",
          },
-
-         "Flax Bundle":{
+         "Flax Bundle": {
             tr: "Keten Bohçası",
          },
-
-         "Salt":{
+         "Salt": {
             tr: "Tuz",
          },
-
-         "Oil":{
+         "Oil": {
             tr: "Yağ",
          },
+         // ===============================================================
+         // ========================== ITEMS END ==========================
+         // ===============================================================
 
-
-         "Profit":{
+         "Profit": {
             tr: "Kazanç"
          },
-         "Percentage Profit":{
+         "Percentage Profit": {
             tr: "Yüzdesel Kazanç"
          },
-         "Pair of values to highlight for flat difference":{
+         "Pair of values to highlight for flat difference": {
             tr: "Kazancın vurgulanacağı değer aralığı"
          },
-         "For percentage difference":{
+         "For percentage difference": {
             tr: "Yüzdesel olan için de ayarlayın"
          },
-         "Language":{
+         "Language": {
             tr: "Dil"
          },
-         "Sort by percentage difference":{
+         "Sort by percentage difference": {
             tr: "Yüzdesel kazanca göre sırala"
          },
-         "Sort by flat difference":{
+         "Sort by flat difference": {
             tr: "Kazanca göre sırala"
          },
-
-
-
-
-
-
-         flatDif: {
-            tr: "Kazanç",
-            en: "Profit"
-         },
-         percentageDif: {
-            tr: "Yüzdesel Kazanç",
-            en: "Percentage Profit",
-         },
-         flatDifLabel:{
-            tr: "Kazancın vurgulanacağı değer aralığı",
-            en: "Pair of values to highlight for flat difference"
-         },
-         percentageDifLabel:{
-            tr: "Yüzdesel olan için de ayarlayın",
-            en: "For percentage difference"
-         },
-         language: {
-            tr: "Dil",
-            en: "Language"
-         },
-         flatSortLabel:{
-            tr: "Kazanca göre sırala",
-            en: "Sort by flat difference"
-         },
-         percentageSortLabel:{
-            tr: "Yüzdesel kazanca göre sırala",
-            en: "Sort by percentage difference"
+         "Some useful links": {
+            tr: "Bazı yararlı linkler"
          }
       }
    },
 
    methods: {
-      init(){
+      init() {
          //ITEM-INIT
          this.itemInit()
          //ITEM-LOOP
-         this.items.forEach(function(item){
+         this.items.forEach(function(item) {
             item.flatDif = item.highest - item.lowest
-            item.percentageDif = Math.round(Number(item.flatDif / item.lowest)*100)
+            item.percentageDif = Math.round(Number(item.flatDif / item.lowest) * 100)
          })
          this.sortRequest('flat')
          this.sortRequest('flat')
       },
-      itemInit(){
+      itemInit() {
          var items = [
             {
                name: "Tools",
@@ -180,63 +147,74 @@ var app = new Vue({
                lowest: 300,
                highest: 550,
                imgPath: "oil.png",
+            },
+            {
+               name: "Iron",
+               lowest: 50,
+               highest: 430,
+               imgPath: "iron.png",
+            },
+            {
+               name: "Raw Silk",
+               lowest: 250,
+               highest: 750,
+               imgPath: "raw-silk.png",
+            },
+            {
+               name: "Furs",
+               lowest: 250,
+               highest: 450,
+               imgPath: "furs.png",
             }
          ]
          this.addItems(items)
       },
-      imgFullPath(path){
+      imgFullPath(path) {
          return "./img/" + path;
       },
-      flatDifTextClass(flatDif){
+      flatDifTextClass(flatDif) {
          var number = Number(flatDif)
          return {
             "item-tooltip-text-low": number < this.flatMin,
             "item-tooltip-text-high": number > this.flatMax
          }
       },
-      percentageDifTextClass(percentageDif){
+      percentageDifTextClass(percentageDif) {
          var number = Number(percentageDif)
          return {
             "item-tooltip-text-low": number < this.percentageMin,
             "item-tooltip-text-high": number > this.percentageMax
          }
       },
-      getString(list){
-         return list[this.language]
-      },
-      translate(str){
-         if(this.language=="en") return str
-         else if(typeof this.dict[str] == 'undefined' || typeof this.dict[str][this.language] == 'undefined'){
+      translate(str) {
+         if (this.language == "en") return str
+         else if (typeof this.dict[str] == 'undefined' || typeof this.dict[str][this.language] == 'undefined') {
             return str
-         }
-         else{
+         } else {
             return this.dict[str][this.language]
          }
       },
-      addItem(item){
+      addItem(item) {
          item.hover = false
          this.items.push(item)
       },
-      addItems(items){
+      addItems(items) {
          items.forEach(this.addItem)
       },
-      sortRequest(requestType){
-         if(requestType == "flat"){
-            if(this.flatSorted){
+      sortRequest(requestType) {
+         if (requestType == "flat") {
+            if (this.flatSorted) {
                this.reverse()
-            }
-            else{
+            } else {
                this.sortBy("flatDif")
                this.flatSorted = true
                this.percentageSorted = false
             }
 
-         }
-         else if(requestType == "percentage"){
-            if(this.percentageSorted){
+         } else if (requestType == "percentage") {
+            if (this.percentageSorted) {
                this.reverse()
-            }
-            else{
+            } else {
                this.sortBy("percentageDif")
                this.flatSorted = false
                this.percentageSorted = true
@@ -245,22 +223,21 @@ var app = new Vue({
          }
          this.sortCounter++
       },
-      sortBy(sortBy){
-         for(var i=0; i<this.items.length; i++){
-            for(var j=0; j<this.items.length-1; j++){
-               if(this.items[j][sortBy] > this.items[j+1][sortBy]){
+      sortBy(sortBy) {
+         for (var i = 0; i < this.items.length; i++) {
+            for (var j = 0; j < this.items.length - 1; j++) {
+               if (this.items[j][sortBy] > this.items[j + 1][sortBy]) {
                   var temp = this.items[j]
-                  this.items[j] = this.items[j+1]
-                  this.items[j+1] = temp
+                  this.items[j] = this.items[j + 1]
+                  this.items[j + 1] = temp
                }
             }
          }
       },
-      reverse(){
+      reverse() {
          this.items = this.items.reverse()
       }
 
    },
-   watch: {
-   }
+   watch: {}
 })
