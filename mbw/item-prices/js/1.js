bc7a84562f4f2c381e3604840a1e74b92b0e4859
okan <okanakar@std.iyte.edu.tr>
2019-09-17 03:49:00 +0300

    Add files via upload

--- mbw/item-prices/js/1.js
+++ mbw/item-prices/js/1.js
@@ -12,10 +12,10 @@ var app = new Vue({
       percentageSorted: false,
       items: [],
 
-      flatMin: 125,
+      flatMin: 70,
       flatMax: 200,
-      percentageMin: 75,
-      percentageMax: 200,
+      percentageMin: 50,
+      percentageMax: 100,
 
       language: "tr",
       dict: {
@@ -49,6 +49,30 @@ var app = new Vue({
          "Oil": {
             tr: "Yağ",
          },
+         "Iron":{
+            tr: "Demir"
+         },
+         "Raw Silk":{
+            tr: "Ham İpek"
+         },
+         "Furs":{
+            tr: "Kürk"
+         },
+         "Spice":{
+            tr: "Baharat"
+         },
+         "Dyes":{
+            tr: "İplik Boyası"
+         },
+         "Wool":{
+            tr: "Yün"
+         },
+         "Date Fruit":{
+            tr: "Hurma"
+         },
+         "Wine":{
+            tr: "Şarap"
+         },
          // ===============================================================
          // ========================== ITEMS END ==========================
          // ===============================================================
@@ -76,6 +100,18 @@ var app = new Vue({
          },
          "Some useful links": {
             tr: "Bazı yararlı linkler"
+         },
+         "Min buy": {
+            tr: "En uygun alış"
+         },
+         "Max sell": {
+            tr: "En kârlı satış"
+         },
+         "Buy from": {
+            tr: "Al"
+         },
+         "Sell to": {
+            tr: "Sat"
          }
       }
    },
@@ -96,75 +132,130 @@ var app = new Vue({
          var items = [
             {
                name: "Tools",
-               lowest: 330,
-               highest: 545,
+               lowest: 380,
+               highest: 430,
                imgPath: "tools.png",
             },
             {
                name: "Linen",
-               lowest: 190,
-               highest: 340,
+               lowest: 220,
+               highest: 270,
+               lowestTerritories: ["Sargoth"],
+               highestTerritories: ["Bariyye"],
                imgPath: "linen.png",
             },
             {
                name: "Leatherwork",
-               lowest: 200,
-               highest: 300,
+               lowest: 190,
+               highest: 240,
                imgPath: "leatherwork.png",
             },
             {
                name: "Hides",
-               lowest: 85,
-               highest: 158,
+               lowest: 90,
+               highest: 130,
                imgPath: "hides.png",
             },
             {
                name: "Wool Cloth",
-               lowest: 132,
-               highest: 450,
+               lowest: 210,
+               highest: 270,
+               lowestTerritories: ["Ahmerrad"],
+               highestTerritories: ["Dhirim"],
                imgPath: "wool-cloth.png",
             },
             {
                name: "Pottery",
-               lowest: 70,
-               highest: 200,
+               lowest: 50,
+               highest: 130,
+               lowestTerritories: ["Sarranid"],
+               highestTerritories: ["Yalen"],
                imgPath: "pottery.png",
             },
             {
                name: "Flax Bundle",
-               lowest: 33,
-               highest: 300,
+               lowest: 110,
+               highest: 200,
+               highestTerritories: ["Tihr", "Durquba"],
                imgPath: "flax-bundle.png",
             },
             {
                name: "Salt",
-               lowest: 50,
-               highest: 280,
+               lowest: 120,
+               highest: 260,
+               lowestTerritories: ["Wercheg", "Tulga"],
+               highestTerritories: ["Jelkala"],
                imgPath: "salt.png",
             },
             {
                name: "Oil",
-               lowest: 300,
-               highest: 550,
+               lowest: 340,
+               highest: 470,
+               lowestTerritories: ["Suno"],
+               highestTerritories: ["Sargoth"],
                imgPath: "oil.png",
             },
             {
                name: "Iron",
-               lowest: 50,
-               highest: 430,
+               lowest: 180,
+               highest: 290,
+               lowestTerritories: ["Curaw"],
+               highestTerritories: ["Rivacheg", "Khudan", "Wercheg"],
                imgPath: "iron.png",
             },
             {
                name: "Raw Silk",
-               lowest: 250,
-               highest: 750,
+               lowest: 300,
+               highest: 650,
+               lowestTerritories: ["Rivacheg", "Vezin"],
+               highestTerritories: ["Jelkala"],
                imgPath: "raw-silk.png",
             },
             {
                name: "Furs",
-               lowest: 250,
-               highest: 450,
+               lowest: 280,
+               highest: 400,
+               lowestTerritories: ["Rivacheg"],
+               highestTerritories: ["Yalen", "Jelkala", "Veluca", "Shariz", "Bariyye"],
                imgPath: "furs.png",
+            },
+            {
+               name: "Spice",
+               lowest: 400,
+               highest: 950,
+               lowestTerritories: ["Tulga", "Khergit"],
+               highestTerritories: ["Nord", "Rhodok", "Swadia", "Vaegir"],
+               imgPath: "spice.png",
+            },
+            {
+               name: "Dyes",
+               lowest: 120,
+               highest: 210,
+               imgPath: "dyes.png",
+            },
+            {
+               name: "Wool",
+               lowest: 85,
+               highest: 190,
+               lowestTerritories: ["Yalen"],
+               highestTerritories: ["Ichamur"],
+               imgPath: "wool.png",
+            },
+            {
+               name: "Date Fruit",
+               lowest: 70,
+               highest: 135,
+               lowestTerritories: ["Sarranid"],
+               highestTerritories: ["Yalen", "Wercheg"],
+               imgPath: "date-fruit.png",
+            },
+            {
+               name: "Wine",
+               lowest: 200,
+               highest: 290,
+               lowestTerritories: ["Veluca"],
+               highestTerritories: ["Bariyye"],
+               imgPath: "wine.png",
             }
          ]
          this.addItems(items)
@@ -196,6 +287,8 @@ var app = new Vue({
       },
       addItem(item) {
          item.hover = false
+         if (typeof item.highestTerritories == 'undefined') item.highestTerritories = []
+         if (typeof item.lowestTerritories == 'undefined') item.lowestTerritories = []
          this.items.push(item)
       },
       addItems(items) {
@@ -236,7 +329,17 @@ var app = new Vue({
       },
       reverse() {
          this.items = this.items.reverse()
-      }
+      },
+      territoryClass(territory){
+         return {
+            rhodok: ["Rhodok", "Jelkala", "Veluca", "Yalen"].includes(territory),
+            nord: ["Nord", "Tihr", "Wercheg", "Sargoth"].includes(territory),
+            vaegir: ["Vaegir", "Rivacheg", "Curaw", "Khudan", "Reyvadin"].includes(territory),
+            swadia: ["Swadia", "Praven", "Uxhal", "Dhirim", "Suno"].includes(territory),
+            khergit: ["Khergit", "Halmar", "Tulga", "Ichamur", "Narra"].includes(territory),
+            sarranid: ["Sarranid", "Shariz", "Durquba", "Ahmerrad", "Bariyye"].includes(territory)
+         }
+      },
 
    },
    watch: {}
