f1d5d246c9e1fb4da3122ce67522499a3096ef52
okan <okanakar_97@hotmail.com>
2020-05-02 17:37:46 +0300

    Add files via upload

--- boss-drop/js/1.js
+++ boss-drop/js/1.js
@@ -3,15 +3,19 @@ var app = new Vue({
    data: {
       tekKesisteDropDusmeIhtimali: 0.01,
       bossKesmeSayisi: 80,
+      tamOlarakXTaneDussun: 1,
       // totaldeDropDusmeIhtimali: 0,
    },
    computed: {
       tekKesisteDropDusmemeIhtimali: function(){
          return 1-this.tekKesisteDropDusmeIhtimali
       },
-      totaldeDropDusmeIhtimali: function(){
+      totaldeEnAz1KereDropDusmeIhtimali: function(){
          return 1-(this.tekKesisteDropDusmemeIhtimali ** this.bossKesmeSayisi)
-      }
+      },
+      sadeceXKezDusmeIhtimali: function(){
+         return (this.tekKesisteDropDusmeIhtimali**this.tamOlarakXTaneDussun)*(this.tekKesisteDropDusmemeIhtimali**(this.bossKesmeSayisi-this.tamOlarakXTaneDussun))*this.kombinasyon(this.bossKesmeSayisi, this.tamOlarakXTaneDussun)
+      },
    },
    methods: {
       hesaplaTotal() {
@@ -20,6 +24,15 @@ var app = new Vue({
          console.log(this.tekKesisteDropDusmemeIhtimali)
          console.log(this.bossKesmeSayisi)
          console.log(this.totaldeDropDusmeIhtimali)
+      },
+      kombinasyon(x,y){
+         let pay = 1;
+         let payda = 1;
+         for(let i = 0; i<y; i++){
+            pay*=x-i
+            payda*=y-i
+         }
+         return  pay/payda
       }
    },
 })
